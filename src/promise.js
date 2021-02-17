e0aad760da33a0509b8447b8200d49d6c5d30c5b
Chris Sheehan <chris_sheehan@hotmail.co.uk>
2021-02-17 16:17:46 +0000

    promises example

--- src/promise.js
+++ src/promise.js
@@ -1,11 +1,45 @@
-const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        console.log("yo")
-        resolve({user: "ed"});
-        //reject(new Error('random message'));
-    }, 2000);
+///https://www.youtube.com/watch?v=DHvZLI7Db8E
+
+const { get, agent } = require("superagent")
+
+let p = new Promise((resolve, reject) => {
+    let a = 1 + 1
+
+    if(a == 2) {
+        resolve('great success :)')
+    }else {
+        reject('failed :(')
+    }
 })
 
-promise.then(user => {
-    console.log(user);
-}).catch(err => console.log(err));
+p.then((message) => {
+    console.log('this is something ' + message)
+}).catch((message) => {
+    console.log('error' + message) 
+})
+
+
+const request = require('supertest');
+
+// with callback
+function getResponse(url) {
+    
+    return request(url)
+        .get('/json?text=ass')
+        .set('Accept', 'application/json')
+        .expect(200)
+} 
+
+function getReplacedText() {
+    return getResponse('https://www.purgomalum.com/service');
+}
+
+getReplacedText()
+.then(response => {
+    console.log(response.body)
+})
+.catch(error => {
+    console.log('error is ' + error)
+})
+
+
