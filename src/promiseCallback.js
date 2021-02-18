0da2fbea202b99ae56dfe4c48c84a0f47db4e1d8
Chris Sheehan <chris_sheehan@hotmail.co.uk>
2021-02-18 10:58:46 +0000

    promise (kind of)

--- src/promiseCallback.js
+++ src/promiseCallback.js
@@ -28,18 +28,35 @@ function getResponse(url) {
         .get('/json?text=ass')
         .set('Accept', 'application/json')
         .expect(200)
-} 
-
-function getReplacedText() {
-    return getResponse('https://www.purgomalum.com/service');
 }
 
-getReplacedText()
-.then(response => {
-    console.log(response.body)
+
+function getReplacedText() {
+
+    return getResponse('https://www.purgomalum.com/service');  
+}
+
+function getReplacedTextBodyResult(something) {
+    
+    console.log(something);
+
+    getReplacedText()
+    .then(response => {
+        console.log(response.body)
+        return response.body.result;
+    })
+    .catch(error => {
+        console.log('error is ' + error)
+    })
+}
+
+//https://stackoverflow.com/questions/11782453/class-methods-in-node-js
+
+
+getReplacedTextBodyResult('adsasas', result => {
+    console.log('result' + result);
 })
-.catch(error => {
-    console.log('error is ' + error)
-})
+
+
 
 
